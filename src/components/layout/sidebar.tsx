243c2a0a9790434499939b359122bc99e6f16310
trinhdinhtai <taitd153.dev@gmail.com>
2024-10-04 23:19:05 +0700

    feat: update search input component

--- src/components/layout/sidebar.tsx
+++ src/components/layout/sidebar.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import SearchInput from "@/components/search-input"
 
 export default function Sidebar() {
@@ -5,7 +7,7 @@ export default function Sidebar() {
     <div className="sticky top-12 mt-12 hidden h-[calc(100vh-3rem)] md:flex">
       <aside className="flex w-64 flex-col p-4">
         {/* Search input */}
-        <SearchInput />
+        <SearchInput onSearch={(term) => {}} clearSearch={() => {}} />
       </aside>
     </div>
   )
--- src/components/search-input.tsx
+++ src/components/search-input.tsx
@@ -1,3 +1,54 @@
-export default function SearchInput() {
-  return <div>SearchInput</div>
+"use client"
+
+import { ChangeEvent, useState } from "react"
+import { Search, X } from "lucide-react"
+
+import { Input } from "@/components/ui/input"
+
+interface SearchInputProps {
+  onSearch: (term: string) => void
+  clearSearch: () => void
+}
+
+export default function SearchInput({
+  onSearch,
+  clearSearch,
+}: SearchInputProps) {
+  const [searchTerm, setSearchTerm] = useState("")
+
+  const handleSearch = (event: ChangeEvent<HTMLInputElement>) => {
+    const term = event.target.value.toLowerCase()
+    setSearchTerm(term)
+    onSearch(term)
+  }
+
+  const handleClear = () => {
+    setSearchTerm("")
+    onSearch("")
+    clearSearch()
+  }
+
+  return (
+    <div className="relative mb-3 ml-3 mr-2">
+      <Input
+        type="text"
+        placeholder="Search..."
+        value={searchTerm}
+        onChange={handleSearch}
+        className="border border-none border-border bg-transparent pl-0 text-sm text-[#606060] text-primary !placeholder-[#606060] outline-none focus:!border-none focus:!ring-0"
+      />
+      {searchTerm ? (
+        <X
+          className="absolute right-0 top-1/2 -translate-y-1/2 cursor-pointer text-[#606060]"
+          size={16}
+          onClick={handleClear}
+        />
+      ) : (
+        <Search
+          className="absolute right-0 top-1/2 -translate-y-1/2 text-[#606060]"
+          size={16}
+        />
+      )}
+    </div>
+  )
 }
